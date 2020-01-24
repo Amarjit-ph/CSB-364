a5e4ed4b05ddfc90e165a4eddeaf87adc7469d23
Amarjit pheiroijam <Amarjitp9@gmail.com>
2020-01-24 10:00:22 +0530

    Update substitution-Cipher-2.cpp

--- substitution-Cipher-2.cpp
+++ substitution-Cipher-2.cpp
@@ -4,64 +4,34 @@ PROGRAM NO.2
 SUBSTITUTION CIPHER
 */
 
-#include<iostream>
-#include<string.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-int main() {
-   cout<<"Enter the message:\n";
-   char msg[100];
-   cin.getline(msg,100); //take the message as input
-   int i, j, length,choice,key;
-   cout << "Enter key: ";
-   cin >> key; //take the key as input
-   length = strlen(msg);
-   cout<<"Enter your choice \n1. Encryption \n2. Decryption \n";
-   cin>>choice;
-   if (choice==1) //for encryption{
-      char ch;
-      for(int i = 0; msg[i] != '\0'; ++i) {
-         ch = msg[i];
-         //encrypt for lowercase letter
-         If (ch >= 'a' && ch <= 'z'){
-            ch = ch + key;
-            if (ch > 'z') {
-               ch = ch - 'z' + 'a' - 1;
+
+int main(){
+    string alph="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890~!@#$%^&*()";
+    string key="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0987654321)(*&^%$#@!~";
+    cout<<"Enter string to cipher : ";
+    string str;
+    char ch;
+   
+    getline(cin,str);
+    for(int i=0;i<str.length();i++){
+        ch = str[i];
+        for(int j=0;j<alph.length();j++){
+            if(ch==alph[j]){
+                str[i]=key[j];
             }
-            msg[i] = ch;
-         }
-         //encrypt for uppercase letter
-         else if (ch >= 'A' && ch <= 'Z'){
-            ch = ch + key;
-            if (ch > 'Z'){
-               ch = ch - 'Z' + 'A' - 1;
+        }
+    }
+   
+    cout<<"Ciphered string : "<<str<<endl;
+    for(int i=0;i<str.length();i++){
+        ch = str[i];
+        for(int j=0;j<key.length();j++){
+            if(ch==key[j]){
+                str[i]=alph[j];
             }
-            msg[i] = ch;
-         }
-      }
-      printf("Encrypted message: %s", msg);
-   }
-   else if (choice == 2) { //for decryption
-      char ch;
-      for(int i = 0; msg[i] != '\0'; ++i) {
-         ch = msg[i];
-         //decrypt for lowercase letter
-         if(ch >= 'a' && ch <= 'z') {
-            ch = ch - key;
-            if(ch < 'a'){
-               ch = ch + 'z' - 'a' + 1;
-            }
-            msg[i] = ch;
-         }
-         //decrypt for uppercase letter
-         else if(ch >= 'A' && ch <= 'Z') {
-            ch = ch - key;
-            if(ch < 'A') {
-               ch = ch + 'Z' - 'A' + 1;
-            }
-            msg[i] = ch;
-         }
-      }
-      cout << "Decrypted message: " << msg;
-   }
+        }
+    }
+    cout<<"Deciphered string : "<<str;
 }
